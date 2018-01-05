e9f7051112a1ad6f89856da479ffc69600d9b093
variy hang <variy@qq.com>
2018-01-05 14:23:19 +0800

    123

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ console.log('>>> ' + process.argv)
 console.log('>>> ' + process.env.NODE_ENV)
 
 module.exports = {
-    debug: process.env.NODE_ENV !== 'production',
+    debug: false, //process.env.NODE_ENV !== 'production',
     port: 3000,
     srcPath:  path.join(__dirname, './src'),
     destPath: path.join(__dirname, './docs/')
